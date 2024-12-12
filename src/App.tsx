a816a670b261a54368e000b86218f11ff46758fc
Filip Papiernik <filippapiernik1999@gmail.com>
2024-12-12 22:37:29 +0100

    feat: Initial version

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,78 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { SamplePage } from './pages';
+/* eslint-disable no-restricted-syntax */
+import { useMemo, useRef } from 'react';
+import AudioTracker from './AudioTracker/AudioTracker';
+
+function computeWordPositions(ref) {
+  const textRef = ref.current;
+
+  if (!textRef) return;
+
+  const positions = [];
+
+  let currentIndex = 0;
+
+  const { childNodes } = textRef;
+
+  for (const node of childNodes) {
+    const { textContent } = node;
+    const textNode = node.firstChild;
+    const words = textContent.split(/\s+/);
+
+    for (const word of words) {
+      if (!word) continue;
+
+      const range = document.createRange();
+
+      range.setStart(textNode, currentIndex);
+      range.setEnd(textNode, currentIndex + word.length);
+
+      const rect = range.getBoundingClientRect();
+
+      positions.push({
+        word,
+        x: rect.left,
+        y: rect.top,
+        width: rect.width,
+        height: rect.height,
+      });
+
+      if (word === words[words.length - 1]) {
+        currentIndex += word.length;
+        continue;
+      }
+
+      const value =
+        word === words[words.length - 1] ? word.length : word.length + 1;
+
+      currentIndex += value; // +1 for the space
+    }
+
+    currentIndex = 0;
+  }
+
+  return positions;
+}
 
 const App = () => {
+  const ref = useRef<HTMLParagraphElement>(null);
+
+  const data = useMemo(() => computeWordPositions(ref), [ref]);
+
+  console.log(data);
+
   return (
-    <div className="app">
-      <Router>
-        <Routes>
-          <Route path="/" element={<SamplePage />} />
-        </Routes>
-      </Router>
+    <div
+      className="app"
+      style={{
+        position: 'relative',
+        isolation: 'isolate',
+        padding: '5em',
+      }}
+    >
+      <div>
+        <div ref={ref} />
+      </div>
+      <AudioTracker data={data} />
     </div>
   );
 };
--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
+import { scan } from 'react-scan'; // import this BEFORE react
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import './index.css';
+import '../dist/output.css';
+
+if (typeof window !== 'undefined') {
+  scan({
+    enabled: true,
+    log: true, // logs render info to console (default: false)
+    alwaysShowLabels: true,
+  });
+}
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
